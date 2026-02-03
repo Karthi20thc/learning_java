0fd74efdddaef09551e5e74dec60566e81b162fd
karthik <karthik.mani@factentry.com>
2026-02-03 15:56:17 +0530

    Remove outdated ArrayList, HashMap, HashSet examples and add new implementations for ArrayList and HashMap

---
+++ my_attempts/Collections_Framework/ArrayList/Feb03.java
@@ -0,0 +1,9 @@
+package my_attempts.Collections_Framework.ArrayList;
+
+
+public class Feb03 {
+        public static void main(String[] args) {
+
+            System.out.println("ArrayList");
+    }
+}
